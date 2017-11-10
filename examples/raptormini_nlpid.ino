ec5c35401f6b1bbc84a36c75f7acffb4d9ea674e
Félix Sánchez-Tembleque <fstdp@hotmail.com>
2017-11-10 14:13:20 +0100

    Update raptormini_nlpid.ino

--- examples/raptormini_nlpid.ino
+++ examples/raptormini_nlpid.ino
@@ -1,4 +1,4 @@
-// followline non linear PID algorithm for Bricolabs Raptor robot
+// followline PID algorithm for Bricolabs Raptor robot
 // Arduino Pro Micro, TB6612 driver, Pololu QTR-8 sensor
 
 // TB6612 driver pinout
@@ -16,51 +16,49 @@ QTRSensorsAnalog qtra((unsigned char[]) {6, 7, 8, 9, 0, 1, 2, 3 }, 8);
 unsigned int IR[8];
 
 // parameters and variables for non linear PID
-const int vmin=60;
-const int vmax=120;
-const float kp=.016;
-const float ki=0.001;
-const float kd=0.01;
-const float kv=0.05;
+const int vmin=80;
+const int vmax=150;
+const float kp=.015;
+const float ki=0.0003;
+const float kd=0.2;
+const float kv=0.07;
 int p,d,u,vbase;
 long i=0;
 int p_old=0;
 boolean on=HIGH;
 
 void setup() {
-
 }
 
 void loop()
 {
+  digitalWrite(STBY, on);
+  
   qtra.read(IR); // read raw sensor values
-  p = -4*IR[0]-3*IR[1]-2*IR[2]-IR[3]+IR[4]+2*IR[5]+3*IR[6]+4*IR[7];
+  
+  p = -7*IR[0]-5*IR[1]-3*IR[2]-IR[3]+IR[4]+3*IR[5]+5*IR[6]+7*IR[7];
   i=i+p;
   d=p-p_old;
   p_old=p;
   if ((p*i)<0) i=0;  // integral windup
 
   u=kp*p+ki*i+kd*d;
-  vbase=vmin+(vmax-vmin)*exp(-kv*abs(u));
-  
-  digitalWrite(STBY, on); // disable standby
-  drive(vbase+u,vbase-u); 
+  vbase=vmin+(vmax-vmin)*exp(-kv*abs(kp*p));
+  //vbase=100;
+  drive(vbase+u,vbase-u);
+ 
 }
 
 void drive(int L, int R) // speed for wheels Left and Right
 {
-  //L=constrain (L, -255, 255); // prevent PWM overflow
-  //R=constrain (R, -255, 255);
+  L=constrain(L,-255,255); // avoid PWM overflow
+  R=constrain(R,-255,255);
   
-  L=L+(255-L)*(L>255)+(-255-L)*(L<-255); // prevent PWM overflow
-  R=R+(255-R)*(R>255)+(-255-R)*(R<-255);
-
   digitalWrite(AIN1, L<0); // switch < and >= if left wheel doesnt spin as expected
   digitalWrite(AIN2, L>=0);
-  //analogWrite(PWMA, abs(L));
-  analogWrite(PWMA, L*(L>=0)-L*(L<0));
+  analogWrite(PWMA, abs(L));
   
   digitalWrite(BIN1, R<0); // switch < and >= if left wheel doesnt spin as expected
   digitalWrite(BIN2, R>=0);
-  analogWrite(PWMA, R*(R>=0)-R*(R<0));
+  analogWrite(PWMB, abs(R));
 }
